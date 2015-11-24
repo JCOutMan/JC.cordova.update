ad4a4aab750fa4d351b23e26161000c311eff5f1
JCOutMan <504804228@qq.com>
2015-11-24 18:40:16 +0800

    调整

--- www/JCUpdate.js
+++ www/JCUpdate.js
@@ -1,5 +1,5 @@
 var exec = require('cordova/exec');
 
 exports.update = function(arg0, success, error) {
-    exec(success, error, "update", "update", [arg0]);
+    exec(success, error, "JCUpdate", "update", [arg0]);
 };
